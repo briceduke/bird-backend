446159cad245a63e2584b896cc0776fc236708f9
Brice Duke <bricewduke@gmail.com>
2022-04-16 22:58:21 -0500

    feat(auth): add auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
+import { Response } from 'express';
+import { User } from 'src/users/models/user.model';
 
-@Controller('auth')
-export class AuthController {}
+import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+
+@Controller("auth")
+export class AuthController {
+	constructor(private readonly authService: AuthService) {}
+
+	@UseGuards(LocalAuthGuard)
+	@Post("login")
+	async login(
+		@Res({ passthrough: true }) res: Response,
+		@CurrentUser() user: User
+	) {
+		await this.authService.login(user, res);
+
+		res.send(user);
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Get()
+	isAuth() {
+		return true;
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Post("logout")
+	logout(@Res({ passthrough: true }) res: Response) {
+		this.authService.logout(res);
+
+		res.json({});
+	}
+}
