bf558bba1d00ab14745180c40c74c9f137e02bfa
Brice Duke <bricewduke@gmail.com>
2022-04-16 21:26:19 -0500

    chore(global): add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-	controllers: [AppController],
-	providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			validationSchema: Joi.object({
+				MONGO_URI: Joi.number().required(),
+				PORT: Joi.number().required(),
+				JWT_SECRET: Joi.string().required(),
+				JWT_EXP: Joi.string().required(),
+			}),
+		}),
+	],
+	providers: [],
 })
 export class AppModule {}
