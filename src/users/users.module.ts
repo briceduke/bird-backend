314be803999303eab2410d369a261430f1ccda37
Brice Duke <bricewduke@gmail.com>
2022-04-16 23:01:42 -0500

    fix(auth): export users service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -13,5 +13,6 @@ import { UsersService } from './users.service';
 	],
 	controllers: [UsersController],
 	providers: [UsersService, UsersRepository],
+	exports: [UsersService],
 })
 export class UsersModule {}
